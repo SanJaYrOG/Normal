d1eec641948d7c37e9e351f62a85cc2a36ad444e
Sanjay Soni <98605487+SanJaYrOG@users.noreply.github.com>
2022-05-05 15:29:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
  module "wills_webserver" {
-   source = "git::ssh://SanJaYrOG@gmail.com/modules.git//modules/webserver?ref=main"
+   source = "git::ssh://SanJaYrOG@github.com/modules.git//modules/webserver?ref=main"
    webserver_name = "Will"
    ami = "ami-0b4946d7420c44be4"
    instance_type = "t2.medium"
