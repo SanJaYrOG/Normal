d8cb80b2b132920b7a384ca3ceb12b287ebaa61a
Sanjay Soni <98605487+SanJaYrOG@users.noreply.github.com>
2022-05-05 13:21:58 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "me-south-1"
+}
+
+resource "aws_vpc" "main" {
+    cidr_blokcidr_block = "10.0.0.0/16"  
+}
+ module "wills_webserver" {
+   source = "../modules/webserver"
+   vpc_id = aws_vpc.main.id 
+   cidr_block = "10.0.0.0/16"
+   webserver_name = "Will"
+   ami = "ami-0b4946d7420c44be4"
+   instance_type = "t2.medium"
+ }
