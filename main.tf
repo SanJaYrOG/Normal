d46ae29330e487b2c264af84b2ed5700a2e8f42b
Sanjay Soni <98605487+SanJaYrOG@users.noreply.github.com>
2022-05-05 13:54:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
     cidr_blokcidr_block = "10.0.0.0/16"  
 }
  module "wills_webserver" {
-   source = "../modules/webserver"
+   source = "git::ssh://SanJaYrOG@gmail.com/modules.git//modules/webserver?ref=main"
    vpc_id = aws_vpc.main.id 
    cidr_block = "10.0.0.0/16"
    webserver_name = "Will"
